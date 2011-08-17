1dd5a5909b931ecfc303a2b41ad84d70c1052b06
blangel <langelb@gmail.com>
2011-08-17 15:17:34 -0400

    * Test case for additional sendEmail property.

--- src/test/java/org/moxie/tama/ConnectorTest.java
+++ src/test/java/org/moxie/tama/ConnectorTest.java
@@ -18,14 +18,25 @@ public class ConnectorTest {
     @Test
     public void testPropertiesFileParsing() {
         List<Profile> profiles = Connector.readProfilesFromPropertyFiles(new File("./src/test/resources"));
-        assertEquals(1, profiles.size());
+        assertEquals(2, profiles.size());
+        
         Profile brianProfile = profiles.get(0);
         assertEquals("brian", brianProfile.getName());
         assertEquals("langelb@gmail.com", brianProfile.getEmailAddress());
         assertEquals(1, brianProfile.getUpdateFrequencyInHours());
+        assertEquals(1, brianProfile.sendEmailFrequencyInHours);
         assertEquals(6, brianProfile.getQueries().length);
         assertEquals(15, brianProfile.getRules().length);
         assertFalse(brianProfile.getRemove());
+
+        Profile megsieProfile = profiles.get(1);
+        assertEquals("megsie", megsieProfile.getName());
+        assertEquals("blangel.hbo@gmail.com", megsieProfile.getEmailAddress());
+        assertEquals(1, megsieProfile.getUpdateFrequencyInHours());
+        assertEquals(2, megsieProfile.sendEmailFrequencyInHours);
+        assertEquals(21, megsieProfile.getQueries().length);
+        assertEquals(12, megsieProfile.getRules().length);
+        assertFalse(megsieProfile.getRemove());
     }
 
 }
